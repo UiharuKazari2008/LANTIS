7a0cdb053bb13cb0a0a458666e2de993958c391c
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2017-06-29 16:12:24 -0400

    Uses command line options

--- web.lantis.bash
+++ web.lantis.bash
@@ -8,39 +8,93 @@ LOCAL_PORT=65500              # Local SSH port to connect back into
 LOCAL_WEBHOST=127.0.0.1       # Server Host to rouute web traffic to
 LOCAL_WEBPORT1=80             # Server Port (HTTP)
 LOCAL_WEBPORT2=443            # Server Port (HTTPS)
-LOCAL_OPEN=0                  # Is your local site ports open? Bypass NAT otherwise
+LOCAL_OPEN=1                  # Is your local site ports open? Bypass NAT otherwise
 
 ### Remote Config - End Point for presentation
-REMOTE_HOST=104.236.243.143   # Server to use
 REMOTE_USER=root              # User to login as
 REMOTE_PORT=22                # SSH port of Remote Server
 REMOTE_WEBPORT1=80            # Server Port (HTTP)
 REMOTE_WEBPORT2=443           # Server Port (HTTPS)
-REMOTE_KILL=1                 # Kill what ever is using port #1
-REMOTE_SETUP=1                # Setup Remote Host
+REMOTE_KILL=0                 # Kill what ever is using port #1
+REMOTE_SETUP=0                # Setup Remote Host
 
 #Keys - Common key shared between hosts
-KEY=/root/lantis.key          # SSH Key to auth with for both directions
+KEY=lantis.key          # SSH Key to auth with for both directions
+
+USAGE(){
+cat << EOF
+Usage:
+
+	Remote Host ===========================================================
+
+	-h  Host to connect to (IP Address or FQDN) (REQUIRED)
+	-p  SSH Port (Default: 22)
+	-u  User to login as (Default: root)
+
+	Local Host ============================================================
+
+	-P  SSH Port (Default: 65500)
+	-U  User to login as (Default: root)
+
+	Router ================================================================
+
+	-H  Local Server (Default: 127.0.0.1)
+	-a  Public Port 1   -A Server Port 1 (Default: 80)
+	-b  Public Port 2   -B Server Port 2 (Default: 443)
+
+	Extra Options =========================================================
+
+	-K  Key
+		Key to use for both directions (Default: lantis.key + .pub)
+		
+	-R  Bypass NAT
+		Use Reverse SSH Tunneling to bypass inbound NAT
+		requirements. Useful if you dont have access to
+		the firewall or router. (Not my fault if you get 
+		in trouble...) (Default: Use Direct Connection)
+
+	-K  Kill Port Access
+		Kill what ever is using port 1 to make way for 
+		this connection (AKA Hijack the port)
+
+	-S  Setup Remote Server
+		This will setup the remote host if auth fails
+		You ssh key for the current user will be used
+
+	-C  Config File (Not accepted yet)
+		Config File to use
+EOF
+}
+
+
+
+while getopts "h:puPUHabABSRKC" opt; do
+  case $opt in
+	h) REMOTE_HOST=${OPTARG};;
+	p) REMOTE_PORT=${OPTARG};;
+	u) REMOTE_USER=${OPTARG};;
+	P) LOCAL_PORT=${OPTARG};;
+	U) LOCAL_USER=${OPTARG};;
+	H) LOCAL_WEBHOST=${OPTARG};;
+	a) LOCAL_WEBPORT1=${OPTARG};;
+	b) LOCAL_WEBPORT2=${OPTARG};;
+	A) REMOTE_WEBPORT1=${OPTARG};;
+	B) REMOTE_WEBPORT2=${OPTARG};;
+	S) REMOTE_SETUP=1;;
+	R) LOCAL_OPEN=0;;
+	K) REMOTE_KILL=1;;
+	C) source ${OPTARG};;
+    \?) echo "[PEBKAC] WTF is -$OPTARG?, thats not a accepted option, Abort"; USAGE; exit 1;;
+    :) echo "[PEBKAC] -$OPTARG requires an argument, Abort"; USAGE; exit 1;;
+  esac
+done
+ if [ $# -lt 1 ]; then USAGE; exit; fi
+
+####################                                                                                                               ####################
 
 #SSH Options - Options used for connection
 COMMON_OPT="-C -2 -o BatchMode=yes -o StrictHostKeyChecking=no -o TCPKeepAlive=yes -o ServerAliveInterval=5 -o ConnectTimeout=15 -o LogLevel=Error"
 LOCAL_OPT="-N -o CompressionLevel=9 -o ExitOnForwardFailure=yes -g -L ${REMOTE_WEBPORT1}:${LOCAL_WEBHOST}:${LOCAL_WEBPORT1} -L ${REMOTE_WEBPORT2}:${LOCAL_WEBHOST}:${LOCAL_WEBPORT2}"
-
-
-
-
-
-
-
-
-####################                                                                                                               ####################
-
-
-
-
-
-
-
 
 echo "= LANTIS EasyLink 2 - Academy City Research ========="
 
