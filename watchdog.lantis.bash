54e1e53c4a31e8819a2f038d8a39ae48af19cc8e
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2017-07-01 00:26:52 -0400

    Fixing LATNIS Router

--- watchdog.lantis.bash
+++ watchdog.lantis.bash
@@ -123,18 +123,16 @@ if  [ ${OPER_MODE} -eq 1 ]; then
 		TEST_CONN_FAILED
 	done
 elif  [ ${OPER_MODE} -eq 2 ]; then
-	while [ $# -gt 9 ]; do
-		TEST_INET_VERIFY; if [ $? -eq 0 ]; then
-			TEST_INET_PASSED
-			{ 
-			TEST_HOST_VERIFY 
-			} || { 
-			TEST_HOST_FAILED 
-			}
-			SELECT_REVERSE; KILL_HOST
-		else
-			TEST_INET_FAILED
-		fi
-		TEST_CONN_FAILED
-	done
+	TEST_INET_VERIFY; if [ $? -eq 0 ]; then
+		TEST_INET_PASSED
+		{ 
+		TEST_HOST_VERIFY 
+		} || { 
+		TEST_HOST_FAILED 
+		}
+		SELECT_REVERSE; KILL_HOST
+	else
+		TEST_INET_FAILED
+	fi
+	TEST_CONN_FAILED
 fi
